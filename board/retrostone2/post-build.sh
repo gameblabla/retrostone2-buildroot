cbf5a1c690c9c0fe079d4aea21125bbbc0d67cba
gameblabla <gameblabla@protonmail.com>
2020-12-10 02:14:27 +0100

    Retrostone 2 basic port.

---
+++ board/retrostone2/post-build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+cp $BINARIES_DIR/boot.scr $TARGET_DIR/boot/boot.scr
+cp $BINARIES_DIR/zImage $TARGET_DIR/boot/zImage
+cp $BINARIES_DIR/sun7i-a20-retrostone2.dtb $TARGET_DIR/boot/sun7i-a20-retrostone2.dtb
