d8d2f72645cafa7f4ef53d2f9c06bce82efef97e
gameblabla <gameblabla@protonmail.com>
2020-12-22 08:07:46 +0100

    Merge Retrostone-2 changes to newest buildroot.

--- board/orangepi/orangepi-pc-plus/boot.cmd
+++ board/orangepi/orangepi-pc-plus/boot.cmd
@@ -1,6 +1,6 @@
 setenv fdt_high ffffffff
 
-setenv bootargs console=ttyS0,115200 earlyprintk root=/dev/mmcblk0p2 rootwait
+setenv bootargs console=ttyS0,115200 earlyprintk root=/dev/mmcblk0p2 rootwait hdmi.audio=EDID:0 disp.screen0_output_mode=1920x1080p60 video=Composite-1:720x480
 
 fatload mmc 0 $kernel_addr_r zImage
 fatload mmc 0 $fdt_addr_r sun8i-h3-orangepi-pc-plus.dtb
