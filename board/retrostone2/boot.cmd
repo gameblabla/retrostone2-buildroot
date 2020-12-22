6a7b701d63b39b1779e6d503b4247cde1e4a90a6
gameblabla <gameblabla@protonmail.com>
2020-12-22 04:33:20 +0100

    Multiple changes : - Add Gmenu2x and its dependencies. - Gmenu2x & OPKs needed several changes done to the rootfs. This includes /media pointing to the system as a hole, a symlink to apps in /mnt, another symlink for truetype, configuration files as well as a few missing icons (poweroff & reboot) and adding a default background for 640x480. - Screen is now set to a perfect 60hz. Thanks citral for the help. This also fixes Triple buffering/vsync as OpenDingux stuff relies on a perfect 60hz refresh rate.

--- board/retrostone2/boot.cmd
+++ board/retrostone2/boot.cmd
@@ -1,4 +1,4 @@
-setenv bootargs console=ttyS0,115200 root=/dev/mmcblk0p1 rootwait rootfstype=ext4 rw video=Unknown-1:640x480-32@60 video=HDMI-A-1:d cma=256M hdmi.audio=EDID:0 disp.screen0_output_mode=1920x1080p60 panic=10 ${extra}
+setenv bootargs console=ttyS0,115200 root=/dev/mmcblk0p1 rootwait rootfstype=ext4 rw video=Unknown-1:640x480-32@60 video=HDMI-A-1:d cma=256M hdmi.audio=EDID:0 disp.screen0_output_mode=640x480p60 panic=10 ${extra}
 ext4load mmc 0 0x49000000 /boot/${fdtfile}
 ext4load mmc 0 0x46000000 /boot/zImage
 env set fdt_high ffffffff
