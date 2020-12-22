c88aea199eaf76f2625ef46c1cc544d820e4e8c5
gameblabla <gameblabla@protonmail.com>
2020-12-22 17:36:56 +0100

    Switch to using NTFS3 driver. Greatly improves reliability and speed.

--- board/retrostone2/post-build.sh
+++ board/retrostone2/post-build.sh
@@ -3,3 +3,5 @@
 cp $BINARIES_DIR/boot.scr $TARGET_DIR/boot/boot.scr
 cp $BINARIES_DIR/zImage $TARGET_DIR/boot/zImage
 cp $BINARIES_DIR/sun7i-a20-retrostone2.dtb $TARGET_DIR/boot/sun7i-a20-retrostone2.dtb
+dd if=/dev/zero of=$BINARIES_DIR/apps.ntfs bs=1 count=0 seek=512M
+mkfs.ntfs -F $BINARIES_DIR/apps.ntfs
