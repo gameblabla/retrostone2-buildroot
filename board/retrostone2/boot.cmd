faf586068a7b51f64e70cf3ae2d56ae4630e9b72
gameblabla <gameblabla@protonmail.com>
2020-12-29 07:09:40 +0100

    Lots of changes : - Now using a brightness daemon. - Onboard buttons are now mapped using gpio_keys. (except for the brightness buttons) - (Try) not to use root anymore in preparation for adding SMB support in the future. - Greatly improved boot times. - Minor changes/fixes

--- board/retrostone2/boot.cmd
+++ board/retrostone2/boot.cmd
@@ -1,4 +1,5 @@
-setenv bootargs console=ttyS0,115200 root=/dev/mmcblk0p1 rootwait rootfstype=ext4 ro video=Unknown-1:640x480-32@60 video=HDMI-A-1:d cma=256M hdmi.audio=EDID:0 disp.screen0_output_mode=640x480p60 panic=10 ${extra}
+setenv bootargs nmi_watchdog=0 quiet net.ifnames=0 root=/dev/mmcblk0p1 rootwait rootfstype=ext4 ro video=Unknown-1:640x480-32@60 video=sunxi:640x480-32@60d,monitor=hdmi,hpd=0,edid=0 cma=256M hdmi.audio=EDID:0 vt.global_cursor_default=0 panic=10 ${extra}
+setenv bootdelay=-2
 ext4load mmc 0 0x49000000 /boot/${fdtfile}
 ext4load mmc 0 0x46000000 /boot/zImage
 env set fdt_high ffffffff
