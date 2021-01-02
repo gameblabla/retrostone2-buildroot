e8adb9c59ab07030f31ced4745d7b6a36fcd9fb5
gameblabla <gameblabla@protonmail.com>
2021-01-02 14:04:18 +0100

    Minor other changes : - Upgrade to U-boot 2020.10. (Not much changes but it works and the patch should ensure it works with newer versions) - Fix resetting refresh issue with reset executable after switching to HDMI.

--- board/retrostone2/boot.cmd
+++ board/retrostone2/boot.cmd
@@ -1,4 +1,4 @@
-setenv bootargs quiet net.ifnames=0 root=/dev/mmcblk0p1 rootwait rootfstype=ext4 ro video=Unknown-1:640x480-32@60 video=HDMI-A-1:d cma=256M panic=10 ${extra}
+setenv bootargs quiet net.ifnames=0 root=/dev/mmcblk0p1 rootwait rootfstype=ext4 ro video=Unknown-1:640x480-32@60 video=HDMI-A-1:d video-mode=sunxi:640x480-32@60,monitor=hdmi,hpd=0,edid=0 cma=256M panic=10 ${extra}
 ext4load mmc 0 0x49000000 /boot/${fdtfile}
 ext4load mmc 0 0x46000000 /boot/zImage
 env set fdt_high ffffffff
